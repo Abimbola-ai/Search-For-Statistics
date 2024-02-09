1fa9e32038e35c4d683b118547dab147f5b41eea
abimbola-ai <aojikutu20@aiip.alueducation.com>
2024-02-09 16:34:29 -0500

    Setting up react app and created structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function HeaderComponent() {
+  return <h1>Search For Statistics</h1>
+}
+function SearchBoxComponent() {
+  return <input type="text" placeholder="Enter any keyword" />
+}
+
+function SearchList() {
+  return (
+    <section className="searchlist">
+      <SearchItem />
+      <SearchItem />
+      <SearchItem />
+    </section>
+  )
+}
+
+const SearchItem = () => {
+  return (
+    <article>
+      <h1>Search title</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
+        fugit.
+      </p>
+      <button type="submit">Click to Search</button>
+    </article>
+  )
+}
+
+function FooterList() {
+  return (
+    <section>
+      <FooterItem />
+      <FooterItem />
+      <FooterItem />
+    </section>
+  )
+}
+
+const FooterItem = () => {
+  return (
+    <article>
+      <h2>Footer title</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <a href="#">Click Me</a>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <>
+    <HeaderComponent />
+    <SearchBoxComponent />
+    <SearchList />
+    <FooterList />
+  </>
+)
