f104f530786d8d587bf24483eb4bc1019f7e2759
abimbola-ai <aojikutu20@aiip.alueducation.com>
2024-02-09 21:57:09 -0500

    used data import

---
+++ src/data.js
@@ -0,0 +1,49 @@
+//Declare data
+export const data = [
+  {
+    icon: 'calculate',
+    title: 'Price Calculator',
+    question:
+      'How much does 1 Eur in 1963 equal in today"s prices? Adjust the rent?',
+    buttonTitle: 'Calculate Price Changes',
+    id: 1,
+  },
+  {
+    icon: 'search',
+    title: 'Name Statistics',
+    question: 'What is the most common name in your area? Time to change? ',
+    buttonTitle: 'Search By Name',
+    id: 2,
+  },
+  {
+    icon: 'monitoring',
+    title: 'Stats Table',
+    question: 'Find detailed tables with time and create your own.',
+    buttonTitle: 'Go to Tables',
+    id: 3,
+  },
+]
+
+export const footerData = [
+  {
+    footerIcon: 'calendar_month',
+    footerTitle: 'Calendar',
+    message: 'See the advance release of calendars for 2024',
+    linkTitle: 'Go to Calendar',
+    id: 1,
+  },
+  {
+    footerIcon: 'campaign',
+    footerTitle: 'Subscribe to news',
+    message: 'Get the latest statistics, releases and publications by email',
+    linkTitle: 'Receive email alerts',
+    id: 2,
+  },
+  {
+    footerIcon: 'api',
+    footerTitle: 'API',
+    message: 'Easily retrieve and integrate statistics with your own system',
+    linkTitle: 'Open API',
+    id: 1,
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,53 +1,84 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { data, footerData } from './data.js'
 
 function HeaderComponent() {
-  return <h1>Search For Statistics</h1>
-}
-function SearchBoxComponent() {
-  return <input type="text" placeholder="Enter any keyword" />
+  return (
+    <div className="heading">
+      <h1 className="title">Search For Statistics</h1>
+      <input
+        className="searchbox"
+        type="text"
+        placeholder="Enter any keyword"
+      />
+    </div>
+  )
 }
 
 function SearchList() {
   return (
     <section className="searchlist">
-      <SearchItem />
-      <SearchItem />
-      <SearchItem />
+      {data.map((d) => {
+        const { icon, title, question, buttonTitle, id } = d
+        return (
+          <SearchItem
+            icon={icon}
+            title={title}
+            question={question}
+            buttonTitle={buttonTitle}
+            key={id}
+          />
+        )
+      })}
     </section>
   )
 }
 
-const SearchItem = () => {
+const SearchItem = (props) => {
+  const { icon, title, question, buttonTitle } = props
   return (
-    <article>
-      <h1>Search title</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
-        fugit.
-      </p>
-      <button type="submit">Click to Search</button>
+    <article className="searchitem">
+      <div className="titlesearch">
+        <span class="material-symbols-outlined">{icon}</span>
+        <h1>{title}</h1>
+      </div>
+
+      <p>{question}</p>
+      <button type="submit">{buttonTitle}</button>
     </article>
   )
 }
 
 function FooterList() {
   return (
-    <section>
-      <FooterItem />
-      <FooterItem />
-      <FooterItem />
+    <section className="footerlist">
+      {footerData.map((f) => {
+        const { footerIcon, footerTitle, message, linkTitle, id } = f
+        return (
+          <FooterItem
+            icon={footerIcon}
+            footerTitle={footerTitle}
+            message={message}
+            linkTitle={linkTitle}
+            key={id}
+          />
+        )
+      })}
     </section>
   )
 }
 
-const FooterItem = () => {
+const FooterItem = (props) => {
+  const { icon, footerTitle, message, linkTitle } = props
   return (
-    <article>
-      <h2>Footer title</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <a href="#">Click Me</a>
+    <article className="footeritem">
+      <div className="titlesearch">
+        <span class="material-symbols-outlined">{icon}</span>
+        <h1>{footerTitle}</h1>
+      </div>
+      <p>{message}</p>
+      <a href="#">{linkTitle}</a>
     </article>
   )
 }
@@ -57,7 +88,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
     <HeaderComponent />
-    <SearchBoxComponent />
     <SearchList />
     <FooterList />
   </>
